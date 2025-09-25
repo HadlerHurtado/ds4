25f530cf03dea8b2e71e587faf6222191666da50
hadler hurtado <hadlertrabajos@gmail.com>
2025-09-25 02:01:39 -0500

    laboratorio 33

---
+++ Laboratorio3/laboratorio33/laboratorio33/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Cliente
+{
+    // Método para calcular el perímetro de un rectángulo
+    public double CalculoPerimetroRectangulo(double lado1, double lado2)
+    {
+        return 2 * (lado1 + lado2);
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Cliente cliente = new Cliente();
+
+        Console.Write("Ingrese el valor del primer lado del rectángulo: ");
+        double lado1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el valor del segundo lado del rectángulo: ");
+        double lado2 = double.Parse(Console.ReadLine());
+
+        double perimetro = cliente.CalculoPerimetroRectangulo(lado1, lado2);
+
+
+        Console.WriteLine("El perímetro del rectángulo es: {0}", perimetro);
+    }
+}

a0a37adb16d389fc7365b566090c11015518fcc2
hadler hurtado <hadlertrabajos@gmail.com>
2025-09-25 18:18:17 -0500

    lab 6-3

---
+++ laboratorio 6/lab 6-3/lab 6-3/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            int[] myNumbers = { 1, 2, 3 };
+            Console.WriteLine(myNumbers[10]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("algo slaio mal, valide el indice del arrglo");
+        }
+        finally
+        {
+            Console.WriteLine("continuacion de la aplicacion, luego del bloque try/catch");
+        }
+    }
+}

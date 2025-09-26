38ed86ef7b1ba56f7caa220fdbefa69ae4ecdc32
hadler hurtado <hadlertrabajos@gmail.com>
2025-09-25 19:15:58 -0500

    lab 7-2

---
+++ laboratorio 7/lab 7-2/lab 7-2/Dado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_7_2
+{
+    internal class Dado
+    {
+        private static Random aleatorio;
+        private int valor;
+
+        public Dado()
+        {
+            aleatorio = new Random();
+        }
+        public void Tirar()
+        {
+            valor = aleatorio.Next(1, 7);
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine("el valor del dado es:" + valor);
+        }
+        public int RetornarValor()
+        {
+            return valor;
+        }
+    }
+}

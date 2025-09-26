c8829454b267db21dd17fe2c917153cdb40515b4
hadler hurtado <hadlertrabajos@gmail.com>
2025-09-25 19:00:38 -0500

    lab 7-1

---
+++ laboratorio 7/laboratorio 7/Program.cs
@@ -0,0 +1,12 @@
+﻿using laboratorio_7;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Banco banco1= new Banco();
+        banco1.Operador();
+        banco1.DepositosTotales();
+        Console.ReadKey();
+    }
+}

a9f40ebbd5f6b3d7717b6d08f33538e655b099e8
hadler hurtado <hadlertrabajos@gmail.com>
2025-09-30 19:53:03 -0500

    lab8-2 terminado

---
+++ laboratorio8/lab8-2/lab8-2/Program.cs
@@ -0,0 +1,62 @@
+﻿
+    public class Cuenta
+    {
+        private string idCuenta;
+        public Cuenta( string prmtIdCuenta)
+        {
+            this.idCuenta=prmtIdCuenta;
+            System.Console.WriteLine(
+                "Cuenta.CalcularIntereses() efecuado para la cuenta {0}",
+                this.idCuenta );
+        }
+        public virtual void CalcularIntereses()
+    {
+        System.Console.WriteLine (
+            "Cuenta CalcularIntereses() efectuando paa la cuenta{0}",
+            this.idCuenta );
+    }
+        public string getIdCuenta()
+        {
+            return this.idCuenta;
+        }
+    }
+    public class CuentaCorriente : Cuenta
+    {
+        public CuentaCorriente(string prmtIdCorriente) : base(prmtIdCorriente)
+        {
+        }
+        public override void CalcularIntereses()
+        {
+            System.Console.WriteLine (
+                "CuentaCorriente.CalcularIntereses() efectuando para "+
+                "la cienta {0}", getIdCuenta());
+        }
+    }
+
+    public class CuentaAhorro : Cuenta
+    {
+        public CuentaAhorro(string prmtIdCuenta) : base(prmtIdCuenta)
+        {
+        }
+
+        public override void CalcularIntereses()
+        {
+            System.Console.WriteLine (
+            "CuentaAhorro.CalcularIntereses() efectuando para"+
+            "la cuenta {0}", getIdCuenta());
+        }
+    }
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string CUENTA = "100";
+
+            Cuenta cuenta = new Cuenta(CUENTA);
+        CuentaCorriente cuentaCorriente= new CuentaCorriente(CUENTA);
+        CuentaAhorro cuentaAhorro= new CuentaAhorro(CUENTA);
+        cuenta.CalcularIntereses();
+        cuentaCorriente.CalcularIntereses();
+        cuentaAhorro.CalcularIntereses();
+    }
+}

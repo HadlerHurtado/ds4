40bfb184524a115487d40e33d79a746606dd9122
hadler hurtado <hadlertrabajos@gmail.com>
2025-10-01 22:44:33 -0500

    lab8-9

---
+++ laboratorio8/lab8-9/lab8-9/Program.cs
@@ -0,0 +1,29 @@
+﻿interface iTemplate
+{
+    public void ponerVariable(string nombre, string var);
+    public void verHtml(string template);
+}
+
+class Template : iTemplate
+{
+    public void ponerVariable(string nombre, string var)
+    {
+        Console.WriteLine("Metodo poner variable {nombre} : {var}");
+    }
+    public void verHtml(string template)
+    {
+        Console.WriteLine(template);
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Template temp1 = new Template();
+        temp1.ponerVariable("var1", "Valor1");
+        temp1.ponerVariable("var2", "Valor2");
+        temp1.ponerVariable("var3", "Valor3");
+        temp1.verHtml("<br>Texto de prueba</br>");
+    }
+}

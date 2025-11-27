a97c177033ff63e831edfb1bc2ecfd1650998808
hadler hurtado <hadlertrabajos@gmail.com>
2025-11-26 20:20:32 -0500

    laboratorio 15-4

---
+++ laboratorio 15/laboratorio 15_4/laboratorio 15_4/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace laboratorio_15_4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Convertir texto a números
+            double num1 = double.Parse(TextBox1.Text);
+            double num2 = double.Parse(TextBox2.Text);
+
+            // Sumar
+            double resultado = num1 + num2;
+
+            // Mostrar resultado en el Label
+            Label1.Text = "La suma es: " + resultado.ToString();
+        }
+    }
+}

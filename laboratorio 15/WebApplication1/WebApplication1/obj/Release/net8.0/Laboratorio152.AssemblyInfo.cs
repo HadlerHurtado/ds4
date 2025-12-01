e76c661ff2bd95d4499d6289072b9f078090656a
hadler hurtado <hadlertrabajos@gmail.com>
2025-11-30 22:52:26 -0500

    laboratorios

--- laboratorio 15/WebApplication1/WebApplication1/obj/Release/net8.0/Laboratorio152.AssemblyInfo.cs
+++ laboratorio 15/WebApplication1/WebApplication1/obj/Release/net8.0/Laboratorio152.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio152")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4827626dbbcc5e33985ccab43b3ad621fea31f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio152")]
